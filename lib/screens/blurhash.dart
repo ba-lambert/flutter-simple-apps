257a2903e29d1246c4d894ca80fc9e5dbb0aefa1
ba-lambert <mugwanezalambert12b@gmail.com>
2024-05-10 23:29:23 +0200

    Add theme

---
+++ lib/screens/blurhash.dart
@@ -0,0 +1,41 @@
+// blurhash.dart
+import 'package:flutter/material.dart';
+
+class BlurHashPackage extends StatefulWidget {
+  final Function(ThemeMode) onThemeChanged; // Callback function
+
+  const BlurHashPackage({Key? key, required this.onThemeChanged})
+      : super(key: key);
+
+  @override
+  State<BlurHashPackage> createState() => _BlurHashPackageState();
+}
+
+class _BlurHashPackageState extends State<BlurHashPackage> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title Goes here'),
+        actions: <Widget>[
+          Switch(
+            value: isDarkMode,
+            onChanged: (isOn) {
+              widget.onThemeChanged(isOn
+                  ? ThemeMode.dark
+                  : ThemeMode.light); // Call the callback function
+            },
+          ),
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
